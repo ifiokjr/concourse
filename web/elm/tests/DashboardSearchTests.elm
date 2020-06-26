8715c4243b893eab03207fb540ab774b67170f98
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-06-26 00:10:10 -0400

    web: structure: add id to job record
    And convert all tests to use the `Data` helpers for constructing jobs.
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- web/elm/tests/DashboardSearchTests.elm
+++ web/elm/tests/DashboardSearchTests.elm
@@ -39,29 +39,10 @@ hasData =
             |> Application.handleCallback
                 (Callback.AllJobsFetched <|
                     Ok
-                        [ { name = "job"
-                          , pipelineName = "pipeline"
-                          , teamName = "team1"
-                          , nextBuild =
-                                Just
-                                    { id = 1
-                                    , name = "1"
-                                    , job = Just (Data.jobId |> Data.withTeamName "team1")
-                                    , status = BuildStatusStarted
-                                    , duration =
-                                        { startedAt = Nothing
-                                        , finishedAt = Nothing
-                                        }
-                                    , reapTime = Nothing
-                                    }
-                          , finishedBuild = Nothing
-                          , transitionBuild = Nothing
-                          , paused = False
-                          , disableManualTrigger = False
-                          , inputs = []
-                          , outputs = []
-                          , groups = []
-                          }
+                        [ Data.job 0 0
+                            |> Data.withPipelineName "pipeline"
+                            |> Data.withTeamName "team1"
+                            |> Data.withNextBuild (Data.jobBuild BuildStatusStarted |> Just)
                         ]
                 )
             |> Tuple.first
